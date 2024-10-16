2b3b07f92bc412148a8c70499616132a45fee652
Thomaz Castro <thmz.castro07@gmail.com>
2024-10-16 10:11:02 -0300

    invertString

--- technical-exercises/src/components/InverterString.vue
+++ technical-exercises/src/components/InverterString.vue
@@ -19,7 +19,23 @@
   const stringInvertida = ref('');
   
   function inverterString() {
-    stringInvertida.value = inputString.value.split('').reverse().join('');
-  }
+    const str = inputString.value; // Acesso à string original
+    let chars = str.split(''); // Converte a string em um array de caracteres
+    let a, b; // Variáveis auxiliares
+    
+    // Itera sobre a primeira metade da string
+    for (let i = 0; i < Math.floor(chars.length / 2); i++) {
+        a = chars[i]; // Armazena o caractere da posição i
+        b = chars[chars.length - 1 - i]; // Armazena o caractere da posição correspondente no final
+
+        // Troca os caracteres
+        chars[i] = b;
+        chars[chars.length - 1 - i] = a;
+    }
+
+    // Converte o array de volta para uma string
+    stringInvertida.value = chars.join(''); // Atribui o resultado a stringInvertida
+}
+
   </script>
   
